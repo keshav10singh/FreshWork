04537755fa47b52642db891cf7f21cd5b78404bd
keshav10singh <keshav10singh@gmail.com>
2018-07-07 17:16:50 +0530

    Added search method and login method in home page

---
+++ src/main/java/com/qakeshav/seleniumconcept/HomePage.java
@@ -0,0 +1,7 @@
+package com.qakeshav.seleniumconcept;
+
+public class HomePage {
+	public void homepage() {
+		System.out.println("This is home page");
+	}
+}
---
+++ src/main/java/com/qakeshav/seleniumconcept/SearchPage.java
@@ -0,0 +1,13 @@
+package com.qakeshav.seleniumconcept;
+
+public class SearchPage {
+
+	public void searchpage() {
+		System.out.println("This is search page");
+	}
+	
+	public void searchpage1() {
+		System.out.println("This is srach poage1");
+	}
+	
+}
