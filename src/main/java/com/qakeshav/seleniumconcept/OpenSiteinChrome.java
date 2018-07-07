74e65d88029dcf5b56d27f4a01cc865cfce1d99a
keshav10singh <keshav10singh@gmail.com>
2018-07-07 16:30:06 +0530

    first git commit for my selenium project

---
+++ src/main/java/com/qakeshav/seleniumconcept/OpenSiteinChrome.java
@@ -0,0 +1,11 @@
+package com.qakeshav.seleniumconcept;
+
+public class OpenSiteinChrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Open website in chrome");
+
+	}
+
+}
---
+++ src/test/java/com/qakeshav/testpackage/TestClass.java
@@ -0,0 +1,10 @@
+package com.qakeshav.testpackage;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Class is to run the test");
+	}
+
+}
